fbd4b6b07fd905ddccfb820af9de37b4024b1927
Guillame Tran <guillaume1.tran@epitech.eu>
2025-10-21 13:17:21 +0900

    init game

---
+++ entities/managers/map_manager.gd
@@ -0,0 +1,46 @@
+@tool
+extends Node2D
+class_name MapManager
+
+@onready var tile = preload("res://entities/tiles/tile.tscn")
+@onready var preview: CollisionShape2D = %Preview
+
+@export_category("Map Settings")
+@export var map_size: Vector2i = Vector2i(10, 10):
+	set(value):
+		var shape = %Preview.shape as RectangleShape2D
+		var total_size_x = tile_size * (value.x + spacing) - spacing
+		var total_size_y = tile_size * (value.y + spacing) - spacing
+		shape.size = Vector2(total_size_x, total_size_y)
+		%Preview.position = Vector2(total_size_x / 2, total_size_y / 2)
+		map_size = value
+@export var tile_size = 16
+@export var spacing = 1
+
+var _tiles := []
+func _ready():
+	#check if in editor or not
+	if Engine.is_editor_hint():
+		return
+	GameGlobal.map_manager = self
+	generate_map()
+	pass
+
+func generate_map():
+	clear_map()
+	for x in range(map_size.x):
+		_tiles.append([])
+		for y in range(map_size.y):
+			var new_tile = tile.instantiate()
+			new_tile.position = Vector2(
+				x * (tile_size + spacing),
+				y * (tile_size + spacing)
+			)
+			add_child(new_tile)
+			_tiles[x].append(new_tile)
+
+func clear_map():
+	for column in _tiles:
+		for tile in column:
+			tile.queue_free()
+	_tiles.clear()
---
+++ globals/game_global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var map_manager: MapManager
